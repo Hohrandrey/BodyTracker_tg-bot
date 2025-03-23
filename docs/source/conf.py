50f0bc7242b23383a208cec53bd3465c6062b2d2
Aswertti <var.yerkhova@gmail.com>
2025-03-23 13:03:18 +0500

    сборка документации

---
+++ docs/source/conf.py
@@ -0,0 +1,34 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+project = 'BodyTracker_tg-bot'
+copyright = '2025, IKBO'
+author = 'IKBO'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',    # Для автоматической генерации документации из кода
+    'sphinx.ext.napoleon',   # Для поддержки Google/Numpy-формата docstrings
+    'sphinx.ext.viewcode',   # Для добавления ссылок на исходный код
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'ru'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/source/modules.rst
@@ -0,0 +1,33 @@
+Modules
+=======
+
+.. automodule:: BodyTracker_tg-bot
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+.. automodule:: bmi_calculator
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+.. automodule:: CaloriesCalculator
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+.. automodule:: meal_button
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+.. automodule:: water_reminder
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+.. automodule:: weight_statistics
+   :members:
+   :undoc-members:
+   :show-inheritance:
+

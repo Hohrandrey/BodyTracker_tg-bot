489b124059228d485c23a553f8921e37b4abc325
Lizok256 <lizoid65535@gmail.com>
2025-04-27 13:51:43 +0300

    прерываем другим способом

--- meal_button.py
+++ meal_button.py
@@ -138,7 +138,7 @@ async def view_meals_handler(update: Update, context: ContextTypes.DEFAULT_TYPE)
     await query.answer()
 
     # Дожидаемся завершения всех операций перед возвратом в главное меню
-    await asyncio.sleep(1)  # Добавим небольшую задержку
-    await start(update, context)
+     # Добавим небольшую задержку
+    await back_to_main_menu(update, context, start)
 
 

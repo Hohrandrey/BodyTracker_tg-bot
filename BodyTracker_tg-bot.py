f8d30baa9f0f48b25915588a5a98e942feeffc7e
Lizok256 <lizoid65535@gmail.com>
2025-04-27 11:52:15 +0300

    Переставила строки

--- BodyTracker_tg-bot.py
+++ BodyTracker_tg-bot.py
@@ -143,9 +143,10 @@ if __name__ == '__main__':
     app = Application.builder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
+    app.add_handler(CallbackQueryHandler(meal_choice_handler, pattern='^(breakfast|lunch|dinner|snack)$'))
     app.add_handler(CallbackQueryHandler(button_handler))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, message_handler))
-    app.add_handler(CallbackQueryHandler(meal_choice_handler, pattern='^(breakfast|lunch|dinner|snack)$'))
+
 
     print("Бот работает...")
     app.run_polling()
